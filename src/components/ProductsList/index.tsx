711c1cee29d111969b176ca39102aeebf6b769e7
Adriano <adriano_alves_2008@hotmail.com>
2023-05-24 19:36:07 -0300

    filtrando produtos pela categoria

--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -1,11 +1,33 @@
 'use client'
 import { useProducts } from '@/hooks/useProducts'
+import ProductCard from '../ProductCard'
+import { styled } from 'styled-components'
+
+const ListContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 256px);
+  grid-gap: 32px;
+  max-width: 100%;
+
+  margin-top: 32px;
+`
 
 interface ProductsListProps {}
 
 function ProductsList(props: ProductsListProps) {
   const { data } = useProducts()
-  return <div>ProductsList</div>
+  return (
+    <ListContainer>
+      {data?.map(product => (
+        <ProductCard
+          key={product.id}
+          title={product.name}
+          price={product.price_in_cents}
+          image={product.image_url}
+        />
+      ))}
+    </ListContainer>
+  )
 }
 
 export default ProductsList
--- src/hooks/useProducts.tsx
+++ src/hooks/useProducts.tsx
@@ -1,27 +1,49 @@
 import { useQuery } from '@tanstack/react-query'
 import axios, { AxiosPromise } from 'axios'
 import { ProductsFetchResponse } from '@/types/productsResponse'
+import { useFilter } from './useFilter'
+import { FilterType } from '@/types/filterTypes'
+import { getCategoryByType } from '@/utils/getCategoryByType'
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL as string
 
-const fetcher = (): AxiosPromise<ProductsFetchResponse> => {
+const fetcher = (query: string): AxiosPromise<ProductsFetchResponse> => {
   return axios.post(API_URL, {
-    query: `
-      query{
-        allProducts {
-          id
-          name
-          price_in_cents
-        }
-      }
-    `
+    query
   })
 }
 
+const mountQuery = (type: FilterType) => {
+  if (type === FilterType.ALL)
+    return `
+  query{
+    allProducts {
+      id
+      name
+      price_in_cents
+      image_url
+    }
+  }
+  `
+  return `
+    query{
+      allProducts(filter:{category: "${getCategoryByType(type)}"}){
+        id
+        name
+        price_in_cents
+        image_url
+        category
+      }
+    }
+  `
+}
+
 export function useProducts() {
+  const { type } = useFilter()
+  const query = mountQuery(type)
   const { data } = useQuery({
-    queryFn: fetcher,
-    queryKey: ['products']
+    queryFn: () => fetcher(query),
+    queryKey: ['products', type]
   })
 
   return {
