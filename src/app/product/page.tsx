190c09fe3ad2ccf3dc4a52f2d1f95470b63eca3e
Adriano <adriano_alves_2008@hotmail.com>
2023-06-01 14:36:09 -0300

    finalizando a página de produto

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,18 +1,37 @@
 'use client'
 import { DefaultPageLayout } from '@/components/defaultPageLayout'
 import * as C from './style'
-import { TiArrowBackOutline } from 'react-icons/ti'
+import { BiShoppingBag } from 'react-icons/bi'
 import { BackBtn } from '@/components/BackButton'
 import { useProduct } from '@/hooks/useProduct'
+import { formatPrice } from '@/utils/formatPrice'
 
 function Product({ searchParams }: { searchParams: { id: string } }) {
   const { data } = useProduct(searchParams.id)
-  console.log(data)
+
   return (
     <DefaultPageLayout>
       <C.Container>
         <BackBtn navigate="/" />
-        <section>Informação do produto</section>
+        <section>
+          <img src={data?.image_url} />
+          <div>
+            <C.ProductInfo>
+              <span>{data?.category}</span>
+              <h2>{data?.name}</h2>
+              <span>{formatPrice(data?.price_in_cents ?? 0)}</span>
+              <p>*Frete grátis para todo o Brasil.</p>
+              <div>
+                <h3>Descrição</h3>
+                <p>{data?.description}</p>
+              </div>
+            </C.ProductInfo>
+            <button>
+              <BiShoppingBag size={20} />
+              Adicionar ao carrinho
+            </button>
+          </div>
+        </section>
       </C.Container>
     </DefaultPageLayout>
   )
