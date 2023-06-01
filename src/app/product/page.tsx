972b92ac04d409e4f2c9e75769626575e2471138
Adriano <adriano_alves_2008@hotmail.com>
2023-06-01 11:10:03 -0300

    criando o hook de produto

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -3,8 +3,11 @@ import { DefaultPageLayout } from '@/components/defaultPageLayout'
 import * as C from './style'
 import { TiArrowBackOutline } from 'react-icons/ti'
 import { BackBtn } from '@/components/BackButton'
+import { useProduct } from '@/hooks/useProduct'
 
-function Product() {
+function Product({ searchParams }: { searchParams: { id: string } }) {
+  const { data } = useProduct(searchParams.id)
+  console.log(data)
   return (
     <DefaultPageLayout>
       <C.Container>
